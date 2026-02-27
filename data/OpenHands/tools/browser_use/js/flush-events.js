ce461037460d694aba94a4605bc614220987ca92
Charlie Jyu <charliejyu88@gmail.com>
2026-02-27 10:27:48 -0600

    Revert "add openhands tool configs to data"
    This reverts commit 2d8f6591e183ea0e61bd836303de7f47c049a0ba.

--- data/OpenHands/tools/browser_use/js/flush-events.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    var events = window.__rrweb_events || [];
-    // Clear browser-side events after flushing
-    window.__rrweb_events = [];
-    return JSON.stringify({events: events});
-})();
